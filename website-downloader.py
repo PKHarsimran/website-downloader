f505b757f685ad9877de2169509fe0f2d5bb4ea9
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2026-03-01 08:18:38 -0400

    Update User-Agent and add request headers

--- website-downloader.py
+++ website-downloader.py
@@ -27,8 +27,19 @@ from urllib3.util import Retry
 LOG_FMT = "%(asctime)s | %(levelname)-8s | %(threadName)s | %(message)s"
 
 DEFAULT_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) "
-    "Gecko/20100101 Firefox/128.0"
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/122.0.0.0 Safari/537.36"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
+    ),
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 
 TIMEOUT = 15  # seconds
